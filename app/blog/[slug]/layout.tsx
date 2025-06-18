77213930e1811b6f91d58044ab5b848309ec6cfa
Leandro Ruel <leandroruel@yonkolab.io>
2025-06-17 21:57:34 -0300

    feat(biome): add formatter and linter with biomejs, also formats the whole thing

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -1,18 +1,14 @@
-import '../../global.css'
-import {Navbar} from '../../components/nav'
-import {Analytics} from '@vercel/analytics/react'
-import {SpeedInsights} from '@vercel/speed-insights/next'
+import "../../global.css";
+import { Navbar } from "../../components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function PostLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+	children,
+}: {
+	children: React.ReactNode;
 }) {
-    return (
-        <>
-            {children}
-        </>
-    )
+	return <>{children}</>;
 }
